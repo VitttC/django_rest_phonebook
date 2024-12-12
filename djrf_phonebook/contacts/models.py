06e601e12d38d9d14f41f4279e1e4e2d794ce6ad
VitttC <vittt.caria@gmail.com>
2024-12-12 19:46:43 +0100

    Added 'Contact' model and serializer

--- djrf_phonebook/contacts/models.py
+++ djrf_phonebook/contacts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+"""
+Contact model to store contacts.
+If a country code is not provided it is supposed to be Italian and +39 will be used.
+"""
+class Contact(models.Model):
+    owner = models.ForeignKey('auth.user', related_name='contacts', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30, default='')
+    country_code = models.CharField(max_length=5, default='+39')
+    phone_number = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
---
+++ djrf_phonebook/contacts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from contacts.models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    Expliciting which fields are required
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    first_name =   serializers.CharField(required=True, allow_blank=False, max_length=30)
+    phone_number = serializers.CharField(required=True, allow_blank=False, max_length=10)
+
+    class Meta:
+        model = Contact
+        fields = ['id', 'first_name', 'last_name', 'country_code', 'phone_number']
+
+    def create(self, validated_data):
+        """
+        Create and return a new "Contact" instance, given the validated data.
+        """
+        return Contact.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing "Contact" instance.
+        """
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.country_code = validated_data.get('country_code', instance.country_code)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)   
+        instance.save()
+        return instance
+    
